6ddeffaffa025d3914d33cd428a2123c13ebb8c2
udaycharan <udaycharan61@gmail.com>
2021-09-16 00:12:42 +0530

    improved form validation

--- index.js
+++ index.js
@@ -4,61 +4,83 @@ const formGet = document.querySelector(".form-container");
 
 
 
-signUpBtn.addEventListener("click", (event)=>{
+signUpBtn.addEventListener("click", (event) => {
 
-               event.preventDefault();
-               let signUpData = []; 
-  
-                const firstName = document.querySelector("#firstname").value;
-                const lastName = document.querySelector("#lastname").value;
-                const email  = document.querySelector("#email").value;
-                const newPassword = document.querySelector("#password-1").value;
-                const confirmPassword = document.querySelector("#password-2").value;
+    event.preventDefault();
+    let signUpData = [];
 
-                if(firstName){
-                    signUpData.push(firstName);
-                }
+    const firstName = document.querySelector("#firstname").value;
+    const lastName = document.querySelector("#lastname").value;
+    const email = document.querySelector("#email").value;
+    const newPassword = document.querySelector("#password-1").value;
+    const confirmPassword = document.querySelector("#password-2").value;
 
-                if(lastName){
-                    signUpData.push(lastName);
-                }
+    if (firstName) {
+        signUpData.push(firstName);
+    }
 
-                if(email){
-                    signUpData.push(email);
-                }
+    else {
+        alert("fields cannot be empty!!");
+    }
 
-                if(newPassword){
-                    signUpData.push(newPassword);
-                }
+    if (lastName) {
+        signUpData.push(lastName);
+    }
 
-                if(confirmPassword){
-                    signUpData.push(confirmPassword);
-                }
-                
+    else {
+        alert("fields cannot be empty!!");
+    }
 
-                appendData(signUpData);
-                location.reload();
+    if (email) {
+        signUpData.push(email);
+    }
 
-            
+    else {
+        alert("fields cannot be empty!!");
+    }
+
+    if (newPassword) {
+        signUpData.push(newPassword);
+    }
+
+    else {
+        alert("fields cannot be empty!!");
+    }
+
+    if (confirmPassword) {
+        signUpData.push(confirmPassword);
+    }
+
+    else {
+        alert("fields cannot be empty!!");
+    }
+
+
+
+
+    appendData(signUpData);
+    location.reload();
+
+
 });
 
 
-function appendData(array){
+function appendData(array) {
 
 
-            //checking if the  key(data) is null and if it is null, adding empty array to it
-        if(localStorage.getItem("data") == null){
-             localStorage.setItem("data", '[]');
+    //checking if the  key(data) is null and if it is null, adding empty array to it
+    if (localStorage.getItem("data") == null) {
+        localStorage.setItem("data", '[]');
 
-        }
-            //getting data object and push the new data into it to append teh data to the array.
-        let oldData = JSON.parse(localStorage.getItem("data"));
-           oldData.push(array);
+    }
+    //getting data object and push the new data into it to append teh data to the array.
+    let oldData = JSON.parse(localStorage.getItem("data"));
+    oldData.push(array);
 
-           //finally setting the object with appended data
+    //finally setting the object with appended data
 
-           localStorage.setItem("data", JSON.stringify(oldData));
-} 
+    localStorage.setItem("data", JSON.stringify(oldData));
+}
 
 
 
